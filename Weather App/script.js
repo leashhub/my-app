062e82b18dc58878f6c18139b0acedf331bb5489
leashhub <leash_prints@outlook.com>
2022-07-09 12:43:03 -0700

    Moved all of my code from Codesandbox to VS
    It appears that nothing is working at the moment

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,9 +1,104 @@
+function showTemperature(response) {
+  let h1 = document.querySelector("h1");
+  let temperature = Math.round(response.data.main.temp);
+  h1.innerHTML = `${temperature} F°`;
+}
+
 function showCity(event) {
   event.preventDefault();
-  let cityInput = document.querySelector("#input-group");
-  alert(`Your password is ${cityInput.value}`);
-  let h2 = document.innerHTML("h2");
-  h2.innerHTML = "${cityInput.value}";
+  let searchInput = document.querySelector("#city-input");
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${searchInput.value}`;
+  let apiKey = "7dd23c682ad66c852628ad2d2b23df92";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}&app=id${apiKey}`).then(showTemperature);
 }
-let showCity = document.querySelector("#password-form");
-passwordForm.addEventListener("submit", showCity);
+
+let citySearch = document.querySelector("#search-input");
+citySearch.addEventListener("submit", showCity);
+
+let now = new Date();
+let h3 = document.querySelector("h3");
+let hour = now.getHours();
+let min = now.getMinutes();
+let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let year = now.getFullYear();
+let date = now.getDate();
+
+h3.innerHTML = `Today is ${day} the ${date}th of ${month} , ${year} at ${hour}:${min} `;
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+/*Previous Homework
+
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100
+  },
+  moscow: {
+    temp: -5,
+    humidity: 20
+  }
+};
+console.log(weather);
+
+let city = prompt("Enter a city");
+city = city.toLowerCase();
+city = city.trim();
+
+if (weather[city]) {
+  let temp = Math.round(weather[city].temp);
+  alert(
+    `It is currently ${temp} C° in ${city} with a humidity of ${weather[city].humidity}%`
+  );
+} else {
+  alert(
+    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+  console.log(city);
+}
+*/
